7a9b9a9b6eae6c506385009d15ccb0db1b3c873b
Lucas Mazza <lucas.mazza@plataformatec.com.br>
2016-11-24 14:50:48 -0200

    Configure the logger to log to stderr, not stdout.
    The builtin unstructured logging to stdout conflicts with the JSON output.

--- config/config.exs
+++ config/config.exs
@@ -25,6 +25,8 @@ config :engine_credo,
   source_code_path: "/code",
   engine_config_file: "/config.json"
 
+config :logger, :console, device: :standard_error
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
