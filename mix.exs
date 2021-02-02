e69d582299812b567b8e103d1d462c65c86ec222
Gustavo Araujo <garaujodev@gmail.com>
2021-02-02 11:51:16 -0300

    Add support for credo v1.3
    - v1.3 -> v1.3.2
    - Categorize new checks
    
    Co-authored-by: wevtimoteo <wevtimoteo@users.noreply.github.com>

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule EngineCredo.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:credo, "1.2.3"},
+      {:credo, "1.3.2"},
       {:poison, "~> 2.2"},
       {:briefly, "~> 0.3", only: :test}
     ]
